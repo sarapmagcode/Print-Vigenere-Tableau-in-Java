2c3e07342d6eaf479b4cf8c38020b379f7fa6fc6
sarapmagcode <85553852+sarapmagcode@users.noreply.github.com>
2022-11-18 19:03:52 +0800

    Update OptimizedApproach.java

--- OptimizedApproach.java
+++ OptimizedApproach.java
@@ -4,6 +4,7 @@ public class OptimizedApproach {
         char[][] mat = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                //different starting letter in each row
                 mat[i][j] = (char) ((char) 'A' + (i + j) % 26);
             }
         }
